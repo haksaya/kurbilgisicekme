611585fa7d8666031e6d807b11595a3efe4a6ffa
haksaya <harunaksaya@marmara.edu.tr>
2018-01-08 16:29:25 +0300

    Hatalar giderildi

---
+++ KurCekme/Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="KurCekme.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="/bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+  <h2>Merkez Bankası</h2>
+  <p>Kur Bilgileri</p>            
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th>Dolar</th>
+        <th>Euro</th>
+        <th>Japon Yeni</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+            <asp:Label ID="dolar" runat="server" Text="Label"></asp:Label></td>
+        <td><asp:Label ID="euro" runat="server" Text="Label"></asp:Label></td>
+        <td><asp:Label ID="yen" runat="server" Text="Label"></asp:Label></td>
+      </tr>
+   
+    </tbody>
+  </table>
+            <asp:Button ID="Button1" runat="server" Text="Güncelle" CssClass="btn btn-success" OnClick="Button1_Click" />
+</div>
+
+    </form>
+</body>
+</html>
---
+++ KurCekme/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace KurCekme
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        void kurcek()
+        {
+            // Bugün (en son iş gününe) e ait döviz kurları için
+            string today = "http://www.tcmb.gov.tr/kurlar/today.xml";
+
+            // 14 Şubat 2013 e ait döviz kurları için
+            string anyDays = "http://www.tcmb.gov.tr/kurlar/201302/14022013.xml";
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(today);
+
+            string USD = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='USD']/BanknoteSelling").InnerXml;
+            string EURO = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='EUR']/BanknoteSelling").InnerXml;
+            string YEN = xmlDoc.SelectSingleNode("Tarih_Date/Currency[@Kod='JPY']/BanknoteSelling").InnerXml;
+
+            dolar.Text = USD.ToString();
+            euro.Text = EURO.ToString();
+            yen.Text = YEN.ToString();
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            kurcek();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            kurcek();
+        }
+    }
+}
